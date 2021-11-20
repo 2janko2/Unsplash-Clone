673ab225e137dcfa584a49485ae5c8c47df63c35
unknown <ivan.khlysta@gmail.com>
2021-11-20 19:34:51 +0200

    CSS normalize was added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,12 +7,12 @@ import logo from "../../assets/images/logo.svg";
 import Search from "../Search/Search";
 
 const Header = (props) => {
-    const [showHeader, setShowHeader] = useState(true);
+    const [showMenu, setShowMenu] = useState(true);
 
     useEffect(() => {
         const handleResize = () => {
-            if (window.innerWidth < 768) setShowHeader(false);
-            else setShowHeader(true);
+            if (window.innerWidth < 768) setShowMenu(false);
+            else setShowMenu(true);
         };
 
         window.addEventListener("resize", handleResize);
@@ -23,34 +23,31 @@ const Header = (props) => {
     return (
         <div className={classes.wrapper}>
             <div
-                className={`${classes.mobileMenu} ${showHeader && classes.mobileMenuActive}`}
-                onClick={() => setShowHeader(!showHeader)}
+                className={`${classes.mobileMenu} ${showMenu && classes.mobileMenuActive}`}
+                onClick={() => setShowMenu(!showMenu)}
             >
                 <span className={classes.mobileMenuItem}></span>
                 <span className={classes.mobileMenuItem}></span>
                 <span className={classes.mobileMenuItem}></span>
             </div>
 
-            {showHeader && (
-                <>
-                    <div className={classes.top}>
-                        <div className={classes.logoWrapper}>
-                            <NavLink to="/">
-                                <img className={classes.logo} src={logo} alt="unsplash logo" />
-                            </NavLink>
-                            <div className={classes.siteNameWrapper}>
-                                <div className={classes.siteName}>Unsplash clone</div>
-                                <div className={classes.slogan}>Photos for everyone</div>
-                            </div>
-                        </div>
-                        <Search />
-                        <NavLink className={classes.favorites} to="/favorites">
-                            Favorites
-                        </NavLink>
+            <div className={classes.top}>
+                <div className={classes.logoWrapper}>
+                    <NavLink to="/">
+                        <img className={classes.logo} src={logo} alt="unsplash logo" />
+                    </NavLink>
+                    <div className={classes.siteNameWrapper}>
+                        <div className={classes.siteName}>Unsplash clone</div>
+                        <div className={classes.slogan}>Photos for everyone</div>
                     </div>
-                    <Navbar />
-                </>
-            )}
+                </div>
+                <Search />
+                <NavLink className={classes.favorites} to="/favorites">
+                    Favorites
+                </NavLink>
+            </div>
+
+            {showMenu && <Navbar />}
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "normalize.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -19,4 +20,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
